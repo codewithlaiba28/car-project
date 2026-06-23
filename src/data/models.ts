8276fa3b9ad78ccfa5a9ce34e041b745618adb9b
codewithlaiba28 <codewithlaiba28@gmail.com>
2026-06-23 21:24:40 +0500

    Add models data file

---
+++ src/data/models.ts
@@ -0,0 +1,50 @@
+export const models = [
+  {
+    id: 1,
+    name: 'AERO X1',
+    price: '$120,000',
+    image: '/model_car_1.png',
+    specs: {
+      zeroToHundred: '2.8s',
+      hp: '750',
+      topSpeed: '320 km/h',
+    },
+    description: 'The Aero X1 redefines hypercar aerodynamics with an aggressive, streamlined silhouette built for pure speed and control.',
+  },
+  {
+    id: 2,
+    name: 'LUX GT',
+    price: '$145,000',
+    image: '/model_car_2.png',
+    specs: {
+      zeroToHundred: '3.1s',
+      hp: '680',
+      topSpeed: '300 km/h',
+    },
+    description: 'A perfect blend of futuristic grand touring luxury and uncompromised track-level performance.',
+  },
+  {
+    id: 3,
+    name: 'VOLT E-R',
+    price: '$160,000',
+    image: '/model_car_3.png',
+    specs: {
+      zeroToHundred: '2.1s',
+      hp: '1020',
+      topSpeed: '350 km/h',
+    },
+    description: 'Our most advanced electric supercar, featuring bleeding-edge battery tech and instantaneous torque.',
+  },
+  {
+    id: 4,
+    name: 'AERO X2 Track',
+    price: '$195,000',
+    image: '/model_car_1.png',
+    specs: {
+      zeroToHundred: '2.5s',
+      hp: '850',
+      topSpeed: '340 km/h',
+    },
+    description: 'Stripped down and tuned up. The Track Edition Aero X2 is built for those who demand the ultimate driving experience.',
+  },
+];
